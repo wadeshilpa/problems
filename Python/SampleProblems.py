76d556e8de9da141440ad68b353592d8dd1ace8d
shilpa <shlp.12129@gmail.com>
2025-11-27 18:34:21 -0700

    new

---
+++ Python/SampleProblems.py
@@ -0,0 +1,8 @@
+# You're given a list of (category, points) pairs. A student may pick up to three books, but no two can be from the same category. 
+# The goal is to choose the combination that yields the highest total points.
+
+# [("Math", 10), ("Science", 8), ("History", 7), ("Math", 6)]
+# Best choice: Math(10) + Science(8) + History(7) = 25
+
+# [("Fiction", 5), ("Fiction", 9), ("Art", 4)]
+# Best choice: Fiction(9) + Art(4) = 13
